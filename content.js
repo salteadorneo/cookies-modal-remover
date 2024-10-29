3e4828fbc0b5893e224e43441aab9655911b1626
Cristian Adán <hola@salteadorneo.dev>
2024-10-29 02:06:31 +0100

    Refactor click button reject

--- content.js
+++ content.js
@@ -1,23 +1,5 @@
-const EXCLUDE_TAGS = [
-  'style',
-  'script',
-  'header',
-  'main',
-  'footer',
-  'img',
-  'input',
-  'button',
-  'svg',
-  'path',
-  'a',
-  'video',
-  'i'
-]
-const EXCLUDE_KEYWORDS = [
-  'ytd', // Youtube
-  'ytp', // Youtube
-  'avatar'
-]
+const ELEMENTS = ['div', 'section', 'iframe', 'aside', 'article']
+const BUTTONS = ['button', 'a', '[role=button]', '[onclick]']
 const KEYWORDS = [
   'cookie',
   'advise',
@@ -28,6 +10,7 @@ const KEYWORDS = [
   'consent',
   'modal',
   'cmp',
+  'epb',
   'nosnippet',
   'adblock',
   'dialog',
@@ -36,101 +19,85 @@ const KEYWORDS = [
   'gtm',
   'fixed',
   'sticky',
-  'absolute'
+  'absolute',
+  'div',
+  'section'
 ]
 const KEYWORDS_GROUPS = [
   ['cookie', 'acepta'],
-  ['cookie', 'policy'],
-  ['publicidad', 'continua', 'socio', 'subs']
+  ['cookie', 'accept'],
+  ['cookie', 'política'],
+  ['publicidad', 'continua', 'socio', 'subs'],
+  ['cookie', 'policy']
+]
+const BUTTON_REJECT = [
+  'rechazar',
+  'no acepta',
+  'continuar sin aceptar',
+  'reject',
+  'deny',
+  'deny all',
+  'non accept',
+  'accept selected',
+  'neccesary cookies only'
+]
+const BUTTON_ACCEPT = [
+  'aceptar',
+  'accept',
+  'aceptar todo',
+  'aceptar todas',
+  'aceptar y cerrar',
+  'aceptar y continuar',
+  'aceptar cookies',
+  'aceptar todo y cerrar',
+  'aceptar y seguir'
 ]
 
-function isRemovableElement (el) {
-  if (EXCLUDE_TAGS.includes(el.tagName.toLowerCase()) ||
-      containsKeyword(EXCLUDE_KEYWORDS, el.tagName) ||
-      Array.from(el.classList).some(value => containsKeyword(EXCLUDE_KEYWORDS, value)) ||
-      Array.from(el.attributes).some(attr => containsKeyword(EXCLUDE_KEYWORDS, attr.value))) {
-    return false
-  }
+async function rejectCookieModal () {
+  const disabled = await getStorageValue('disabled') || false
+  if (disabled) return
 
-  const tagContains = containsKeyword(KEYWORDS, el.tagName)
-  const classesContains = Array.from(el.classList).some(value => containsKeyword(KEYWORDS, value))
-  const attributesContains = Array.from(el.attributes).some(attr => containsKeyword(KEYWORDS, attr.value))
+  let count = 0
+  const elements = Array.from(document.body.querySelectorAll(ELEMENTS.join(',')))
+  elements.forEach(el => {
+    const content = el.textContent.toLowerCase().trim()
+    const hasGroupKeywordsOnContent = KEYWORDS_GROUPS.some(group => group.every(keyword => content.includes(keyword)))
 
-  const content = el.textContent.toLowerCase().trim()
-  const hasKeywordsOnContent = KEYWORDS_GROUPS.some(group => group.every(keyword => content.includes(keyword)))
-  const isOverlay = content.length === 0 && el.children.length === 0 && hasTransparentBackground(el)
+    if (hasElementKeywords(el, KEYWORDS) && hasGroupKeywordsOnContent) {
+      el.querySelectorAll(BUTTONS).forEach(button => {
+        const content = button.textContent.toLowerCase().trim()
+        const hasReject = BUTTON_REJECT.some(keyword => content.includes(keyword))
+        if (hasReject) {
+          button.click()
+          count++
+          return
+        }
+        const hasAccept = BUTTON_ACCEPT.some(keyword => content.includes(keyword))
+        if (hasAccept) {
+          button.click()
+          count++
+        }
+      })
+    }
+  })
+  chrome.runtime.sendMessage({ count })
+}
 
-  const hasFloatingChild = Array.from(el.children).some(child => hasPositionFixedOrAbsolute(child))
-  const isFloating = hasPositionFixedOrAbsolute(el) || hasFloatingChild
-
-  return (tagContains || classesContains || attributesContains) && (hasKeywordsOnContent || isOverlay) && isFloating
+function hasElementKeywords (el, keywords) {
+  return containsKeyword(keywords, el.tagName) ||
+    Array.from(el.classList).some(value => containsKeyword(keywords, value)) ||
+    Array.from(el.attributes).some(attr => containsKeyword(keywords, attr.value)) ||
+    Array.from(el.attributes).some(attr => containsKeyword(keywords, attr.name))
 }
 
 function containsKeyword (array, str) {
   return array.some(value => str.toLowerCase().trim().includes(value))
 }
 
-function hasPositionFixedOrAbsolute (el) {
-  const position = window.getComputedStyle(el).getPropertyValue('position')
-  return ['fixed', 'absolute'].includes(position)
-}
-
-function hasTransparentBackground (el) {
-  const bgColor = window.getComputedStyle(el).backgroundColor
-  const rgba = bgColor.match(/rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d*\.?\d*))?\)/)
-  if (rgba) {
-    const alpha = rgba[4] ? parseFloat(rgba[4]) : 1
-    return alpha < 1
-  }
-  return false
-}
-
-function hasOverflowHidden (el) {
-  const overflow = window.getComputedStyle(el).getPropertyValue('overflow')
-  return overflow === 'hidden'
-}
-
-async function removeCookieModal () {
-  const disabled = await getStorageValue('disabled') || false
-  // if (disabled) return
-
-  const elements = Array.from(document.body.querySelectorAll('*'))
-  let count = 0
-
-  elements.forEach(el => {
-    if (isRemovableElement(el)) {
-      if (!disabled) {
-        el.remove()
-        count++
-      }
-      console.log('Removed', el)
-    }
-  })
-
-  chrome.runtime.sendMessage({ count })
-}
-
-const observer = new globalThis.MutationObserver(() => removeCookieModal())
-observer.observe(document.body, { childList: true, subtree: true })
-
-removeCookieModal()
-
-async function checkBody () {
-  const disabled = await getStorageValue('disabled') || false
-  if (disabled) return
-
-  if (hasOverflowHidden(document.body)) {
-    document.body.style.overflow = 'auto'
-  }
-
-  const bodyClassesToRemove = Array.from(document.body.classList).filter(className => KEYWORDS.some(keyword => className.toLowerCase().includes(keyword)))
-  bodyClassesToRemove.forEach(className => document.body.classList.remove(className))
-}
-
-const bodyObserver = new globalThis.MutationObserver(() => checkBody())
-bodyObserver.observe(document.body, { attributes: true, attributeFilter: ['style', 'class'] })
-
-checkBody()
+rejectCookieModal()
+setTimeout(rejectCookieModal, 500)
+setTimeout(rejectCookieModal, 1000)
+setTimeout(rejectCookieModal, 2000)
 
 async function getStorageValue (key) {
   return new Promise((resolve) => {
@@ -138,15 +105,4 @@ async function getStorageValue (key) {
       resolve(result[key])
     })
   })
-}
-
-document.addEventListener('scroll', disconnectObservers)
-document.addEventListener('click', disconnectObservers)
-
-function disconnectObservers () {
-  observer.disconnect()
-  bodyObserver.disconnect()
-
-  document.removeEventListener('scroll', disconnectObservers)
-  document.removeEventListener('click', disconnectObservers)
 }
